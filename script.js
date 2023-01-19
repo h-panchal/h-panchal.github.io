0049a54ec9a4430aa514c52c49db07fd7175790f
h-panchal <hemal_panchal@outlook.com>
2023-01-19 17:39:28 +0000

    Home Page
    Fin Home page with typewriter effect , made subpages and cleaned up the look.

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
-// Define an array of elements
-var elements = [
-    document.querySelector('a[href="#home"]'),
-    document.querySelector('a[href="#about"]'),
-    document.querySelector('a[href="#projects"]'),
-    document.querySelector('a[href="#contact"]')
-  ];
-  
-  // Loop through the array of elements
-  elements.forEach(function(element) {
-    // Attach a click event handler to each element
-    element.addEventListener('click', function() {
-      // Change the background color of the element to red
-      element.style.backgroundColor = '#aa1704';
-  
-      // Reset the background color of the other elements
-      elements.forEach(function(otherElement) {
-        if (otherElement !== element) {
-          otherElement.style.backgroundColor = '';
-        }
-      });
-    });
-  });
-  
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap {border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
